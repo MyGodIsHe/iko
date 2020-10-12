f62d85c98de934c0b33e55704ee5f72a2584d8ab
Pavel <pollydrag@yandex.ru>
2020-10-12 17:59:52 +0300

    v0.4.0a (#15)

--- iko.py
+++ iko.py
@@ -1,11 +1,19 @@
 import asyncio
-from typing import Dict
-from typing import Type
+import inspect
+import typing
 
 OPTIONAL = object()
+EXCLUDE = 'exclude'
+INCLUDE = 'include'
+
+_TField = typing.Union['Field', typing.Type['Field']]
+_TFields = typing.Dict[str, _TField]
+_TSchema = typing.Union['Schema', typing.Type['Schema']]
 
 
 class Field:
+    OPTIONAL_LIST: typing.Iterable = (OPTIONAL,)
+
     def __init__(
             self,
             default=OPTIONAL,
@@ -15,14 +23,20 @@ class Field:
     ):
         if outer_name is not None:
             assert dump_to is None and load_from is None
-        self.default = default
+        self._default = default
         self.dump_to = dump_to or outer_name
         self.load_from = load_from or outer_name
 
+    @property
+    def default(self):
+        if callable(self._default):
+            return self._default()
+        return self._default
+
     async def dump(self, data, attr, context):
         value = data.get(attr, self.default)
-        if value == OPTIONAL:
-            return value
+        if value in self.OPTIONAL_LIST:
+            return OPTIONAL
         return await self.post_dump(value, context)
 
     async def post_dump(self, value, context):
@@ -30,24 +44,43 @@ class Field:
 
     async def load(self, data, attr, context):
         value = data.get(attr, self.default)
-        if value == OPTIONAL:
-            return value
+        if value in self.OPTIONAL_LIST:
+            return OPTIONAL
         return await self.post_load(value, context)
 
     async def post_load(self, value, context):
         return value
 
 
+class Const(Field):
+    def __init__(self, value, dump_to=None, load_from=None, outer_name=None):
+        self.value = value
+        super().__init__(
+            dump_to=dump_to, load_from=load_from, outer_name=outer_name,
+        )
+
+    async def dump(self, data, attr, context):
+        return self.value
+
+    async def load(self, data, attr, context):
+        return self.value
+
+
 class Nested(Field):
+    schema: 'Schema'
+
     def __init__(
             self,
-            schema: Type['Schema'],
+            schema,
             default=OPTIONAL,
             dump_to=None,
             load_from=None,
             outer_name=None,
     ):
-        self.schema = schema
+        if inspect.isclass(schema):
+            self.schema = schema()
+        else:
+            self.schema = schema
         super().__init__(default, dump_to, load_from, outer_name)
 
     async def post_dump(self, value, context):
@@ -58,22 +91,39 @@ class Nested(Field):
 
 
 class List(Field):
+    """
+    If a field is passed, then only post methods are used.
+    """
+
     def __init__(
             self,
-            schema: Type['Schema'] = None,
+            schema: typing.Optional[_TSchema] = None,
+            field: typing.Optional[_TField] = None,
             default=OPTIONAL,
             dump_to=None,
             load_from=None,
             outer_name=None,
     ):
-        self.schema = schema
+        assert not (schema and field)
+        self.obj_dump = None
+        self.obj_load = None
+        if schema:
+            if inspect.isclass(schema):
+                schema = schema()  # type: ignore
+            self.obj_dump = schema.dump
+            self.obj_load = schema.load
+        if field:
+            if inspect.isclass(field):
+                field = field()  # type: ignore
+            self.obj_dump = field.post_dump  # type: ignore
+            self.obj_load = field.post_load  # type: ignore
         super().__init__(default, dump_to, load_from, outer_name)
 
     async def post_dump(self, value, context):
         return [
             (
-                await self.schema.dump(item, context=context)
-                if self.schema
+                await self.obj_dump(item, context=context)
+                if self.obj_dump
                 else item
             )
             for item in value
@@ -82,8 +132,8 @@ class List(Field):
     async def post_load(self, value, context):
         return [
             (
-                await self.schema.load(item, context=context)
-                if self.schema
+                await self.obj_load(item, context=context)
+                if self.obj_load
                 else item
             )
             for item in value
@@ -96,38 +146,87 @@ class SchemaMeta(type):
         for base in bases:
             if issubclass(base, Schema):
                 fields.update(base.__fields__)
-        fields.update(
-            {
-                name: field
-                for name, field in attrs.items()
-                if isinstance(field, Field)
-            },
-        )
+        for key, field in attrs.items():
+            if isinstance(field, Field):
+                fields[key] = field
+            elif (
+                inspect.isclass(field)
+                and issubclass(field, Field)
+                and field.__name__ != key
+            ):
+                fields[key] = field()
         attrs['__fields__'] = fields
-        return super().__new__(mcs, name, bases, attrs)
+        klass = super().__new__(mcs, name, bases, attrs)
+
+        meta = getattr(klass, 'Meta')
+        klass.__opts__ = klass.OPTIONS_CLASS(meta)
+        return klass
+
+
+class SchemaOpts:
+    def __init__(self, meta):
+        self.unknown = getattr(meta, 'unknown', EXCLUDE)
+        self.exclude = getattr(meta, 'exclude', tuple())
 
 
 class Schema(metaclass=SchemaMeta):
-    __fields__: Dict[str, Field]
+    OPTIONS_CLASS = SchemaOpts
+
+    __fields__: typing.Dict[str, Field]
+    __opts__: SchemaOpts
+
+    class Meta:
+        pass
 
     @classmethod
-    async def dump(cls, data, *, context=None):
+    async def dump(cls, data, *, only=None, exclude=None, context=None):
         if context is None:
             context = {}
-        values = await asyncio.gather(
-            *[
-                field.dump(data, attr, context)
-                for attr, field in cls.__fields__.items()
-            ],
-        )
-        attrs = [
-            field.dump_to or attr for attr, field in cls.__fields__.items()
+
+        data = await cls.pre_dump(data, context)
+
+        only = only or cls.__fields__
+        exclude = exclude or []
+        exclude.extend(cls.__opts__.exclude)
+
+        partial = set(cls.__fields__) & set(only) - set(exclude)
+        fields = [
+            (attr, field)
+            for attr, field in cls.__fields__.items()
+            if attr in partial
         ]
-        return {
+        fields_coros = [
+            field.dump(data, attr, context) for attr, field in fields
+        ]
+        values = await cls.gather(fields_coros, context)
+        attrs = [field.dump_to or attr for attr, field in fields]
+        result = {
             attr: value
             for attr, value in zip(attrs, values)
             if value != OPTIONAL
         }
+        if cls.__opts__.unknown == INCLUDE:
+            known_fields = set(cls.__fields__) | set(exclude)
+            result.update(
+                {
+                    key: value
+                    for key, value in data.items()
+                    if key not in known_fields
+                },
+            )
+        return await cls.post_dump(result, context)
+
+    @classmethod
+    async def gather(cls, fields: typing.List[typing.Coroutine], context):
+        return await asyncio.gather(*fields)
+
+    @classmethod
+    async def pre_dump(cls, value, context):
+        return value
+
+    @classmethod
+    async def post_dump(cls, value, context):
+        return value
 
     @classmethod
     def dump_many(cls, items, *, context=None):
@@ -138,24 +237,55 @@ class Schema(metaclass=SchemaMeta):
         )
 
     @classmethod
-    async def load(cls, data, *, context=None):
+    async def load(cls, data, *, only=None, exclude=None, context=None):
         if context is None:
             context = {}
-        values = await asyncio.gather(
-            *[
-                field.load(
-                    data,
-                    field.load_from if field.load_from else attr,
-                    context,
-                )
-                for attr, field in cls.__fields__.items()
-            ],
-        )
-        return {
+
+        data = await cls.pre_load(data, context)
+
+        only = only or cls.__fields__
+        exclude = exclude or []
+        exclude.extend(cls.__opts__.exclude)
+
+        partial = set(cls.__fields__) & set(only) - set(exclude)
+        fields = [
+            (attr, field)
+            for attr, field in cls.__fields__.items()
+            if attr in partial
+        ]
+        fields_coros = [
+            field.load(
+                data, field.load_from if field.load_from else attr, context,
+            )
+            for attr, field in fields
+        ]
+        values = await cls.gather(fields_coros, context)
+        result = {
             attr: value
-            for attr, value in zip(cls.__fields__, values)
+            for attr, value in zip([field[0] for field in fields], values)
             if value != OPTIONAL
         }
+        if cls.__opts__.unknown == INCLUDE:
+            known_fields = {
+                field.load_from if field.load_from else attr
+                for attr, field in cls.__fields__.items()
+            } | set(exclude)
+            result.update(
+                {
+                    key: value
+                    for key, value in data.items()
+                    if key not in known_fields
+                },
+            )
+        return await cls.post_load(result, context)
+
+    @classmethod
+    async def pre_load(cls, value, context):
+        return value
+
+    @classmethod
+    async def post_load(cls, value, context):
+        return value
 
     @classmethod
     def load_many(cls, items, *, context=None):
@@ -164,3 +294,13 @@ class Schema(metaclass=SchemaMeta):
         return asyncio.gather(
             *[cls.load(item, context=context) for item in items],
         )
+
+
+def schema_from_dict(*args: _TFields, **fields: _TField):
+    all_fields: _TFields = {}
+    for arg in args:
+        all_fields.update(arg)
+    all_fields.update(fields)
+    return typing.cast(
+        typing.Type[Schema], type('DictSchema', (Schema,), all_fields),
+    )
